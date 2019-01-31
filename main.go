bea3a784b7c09cf72aa4c73cad376346e3e75d61
Eaton Emmerich <eemmerich@luno.com>
2019-01-31 11:34:01 +0200

    Add Outputter with STDOUT implementation

--- main.go
+++ main.go
@@ -121,7 +121,7 @@ func validateAll(proc *Processor, files []string) {
 	}
 }
 
-func rewriteOne(proc *Processor, file string) error {
+func rewriteOne(proc *Processor, file string, outputter Outputter) error {
 	// Get the rewritten file.
 	r, err := func() (io.Reader, error) {
 		f, err := os.Open(file)
@@ -137,12 +137,7 @@ func rewriteOne(proc *Processor, file string) error {
 
 	if r != nil {
 		// Write the result.
-		f, err := os.Create(file)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		_, err = io.Copy(f, r)
+		outputter.Write(file, r)
 		if err != nil {
 			return err
 		}
@@ -151,9 +146,9 @@ func rewriteOne(proc *Processor, file string) error {
 	return nil
 }
 
-func rewriteAll(proc *Processor, files []string) {
+func rewriteAll(proc *Processor, files []string, outputter Outputter) {
 	for _, file := range files {
-		err := rewriteOne(proc, file)
+		err := rewriteOne(proc, file, outputter)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(statusError)
@@ -163,6 +158,7 @@ func rewriteAll(proc *Processor, files []string) {
 
 func main() {
 	rewrite := false
+	inplace := true
 	gr := newGrouper()
 
 	flag.Usage = func() {
@@ -194,6 +190,9 @@ Usage: group-imports [OPTIONS] FILE...
 
 	flag.BoolVar(&rewrite, "rewrite", false, "")
 	flag.Var(gr, "order", "")
+	flag.BoolVar(&inplace, "inplace", true, "")
+
+
 
 	flag.Parse()
 	if flag.NArg() == 0 {
@@ -204,7 +203,12 @@ Usage: group-imports [OPTIONS] FILE...
 
 	proc := NewProcessor(gr)
 	if rewrite {
-		rewriteAll(proc, flag.Args())
+		if inplace {
+			rewriteAll(proc, flag.Args(), InplaceOutputter{})
+		} else {
+			rewriteAll(proc, flag.Args(), STDOUTOutputter{})
+		}
+
 	} else {
 		validateAll(proc, flag.Args())
 	}
---
+++ output.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+type Outputter interface {
+	Write(file string, r io.Reader) error
+}
+
+type InplaceOutputter struct {
+}
+
+func (InplaceOutputter) Write(file string, r io.Reader) error{
+	f, err := os.Create(file)
+	if err != nil {
+		return  err
+	}
+	defer f.Close()
+	_, err = io.Copy(f, r)
+	return err
+}
+
+type STDOUTOutputter struct {
+}
+
+func (STDOUTOutputter) Write(file string, r io.Reader) error {
+	_, err := io.Copy(os.Stdout, r)
+	return err
+}
